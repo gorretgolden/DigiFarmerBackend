9d9a7e537f7820b69c1ee7591405947ef9a8fddd
gorretgolden <nabatanzigorret143@gmail.com>
2023-04-20 06:36:11 +0300

    added acrt items table

---
+++ database/migrations/2023_04_20_053024_create_cart_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->foreignId('cart_id')->nullable()->constrained()->onDelete('CASCADE');
+            $table->foreignId('seller_product_id')->nullable()->constrained()->onDelete('CASCADE');
+            $table->foreignId('animal_feed_id')->nullable()->constrained()->onDelete('CASCADE');
+            $table->foreignId('rent_vendor_service_id')->nullable()->constrained()->onDelete('CASCADE');
+            $table->integer('quantity')->default(1);
+            $table->integer('charge_value')->nullable();
+            $table->integer('total_cost');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_items');
+    }
+}
