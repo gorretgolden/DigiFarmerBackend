062deccd3884a0aab4b4f8e05b3f9ca4a3b64383
Nicholas <gidudunicholas11@gmail.com>
2023-05-17 12:27:56 +0300

    Added payment method selection and fething order history for a Client

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -11,8 +11,6 @@ use App\Http\Requests\OrderRequest;
 use Illuminate\Http\Request;
 use DB;
 
-
-
 class OrderController extends Controller
 {
     /**
@@ -22,17 +20,26 @@ class OrderController extends Controller
      */
     public function index(Request $request)
     {
-        try{
-            $data = Orders::where(['user_id' => $request->user()->id])->with('orderDetails.cart.product')->get();
+        try {
+            $data = Orders::where(["user_id" => $request->user()->id])
+                ->with("orderDetails.cart.product")
+                ->get();
 
-            return response()->json(['success'=>true,'message'=>'orders fetched successfully', 'data'=>$data, 'error'=>null]);
-        }catch(Exception $e){
-            return response()->json(['success'=>false,'message'=>'Error processing orders','data'=>null, 'error'=>$e->getMessage()]);
+            return response()->json([
+                "success" => true,
+                "message" => "orders fetched successfully",
+                "data" => $data,
+                "error" => null,
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                "success" => false,
+                "message" => "Error processing orders",
+                "data" => null,
+                "error" => $e->getMessage(),
+            ]);
         }
-
-
     }
-
 
     /**
      * Store a newly created resource in storage.
@@ -42,47 +49,45 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-
         $cart = CartItem::all();
 
-        if(!$cart->isEmpty()){
-           foreach ($cart as $cart_data) {
-             $save_data[] = [
-             'id' => $cart_data['id'],
-             'cart_id' => $cart_data['cart_id'],
-             'vendor_service_id' => $cart_data['vendor_service_id'],
-             'quantity' => $cart_data['quantity'],
-             'type' => $cart_data['type'],
-             'total_cost' => $cart_data['total_cost'],
-             'user_id' => $cart_data['user_id'],
-             ];
+        if (!$cart->isEmpty()) {
+            foreach ($cart as $cart_data) {
+                $save_data[] = [
+                    "id" => $cart_data["id"],
+                    "cart_id" => $cart_data["cart_id"],
+                    "vendor_service_id" => $cart_data["vendor_service_id"],
+                    "quantity" => $cart_data["quantity"],
+                    "type" => $cart_data["type"],
+                    "total_cost" => $cart_data["total_cost"],
+                    "user_id" => $cart_data["user_id"],
+                ];
             }
 
+            DB::table("cart_pivots")->insert($save_data);
+            $orderModel = new Orders();
+            $orderModel->payment_method = $request->payment_method;
+            $orderModel->total_amount = $request->total_amount;
+            $orderModel->user_id = $request->user()->id;
+            $orderModel->payment_status = "unpaid";
+            $orderModel->id = mt_rand(1000000000, 9999999999);
+            $orderModel->amount_paid = $request->amount_paid;
+            $orderModel->transaction_ref = $request->transaction_ref;
+            $orderModel->external_ref = $request->external_ref;
+            $orderModel->save();
+            foreach ($cart as $cartData) {
+                $orderdetail = new orderDetail();
+                $orderdetail->cart_pivot_id = $cartData->id;
+                $orderModel->orderDetails()->save($orderdetail);
+            }
 
-
-DB::table('cart_pivots')->insert($save_data);
-$orderModel = new Orders();
-$orderModel->payment_method = 'mobile_money';
-$orderModel->total_amount = '3000';
-$orderModel->user_id = $request->user()->id;
-$orderModel->payment_status = "unpaid";
-$orderModel->id = mt_rand(1000000000,9999999999);
-$orderModel->amount_paid = $request->amount_paid;
-$orderModel->transaction_ref = $request->transaction_ref;
-$orderModel->external_ref = $request->external_ref;
-$orderModel->save();
-foreach ($cart as $cartData) {
-$orderdetail = new orderDetail;
-$orderdetail->cart_pivot_id = $cartData->id;
-$orderModel->orderDetails()->save($orderdetail);
-};
-
-CartItem::where(['user_id'=> $request->user()->id])->delete();
-
-}else{
-return response()->json(['message'=>'You add some items to cart'], 402);
-}
-
+            CartItem::where(["user_id" => $request->user()->id])->delete();
+        } else {
+            return response()->json(
+                ["message" => "You add some items to cart"],
+                402
+            );
+        }
     }
 
     /**
