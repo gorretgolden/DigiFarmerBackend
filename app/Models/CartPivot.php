1795924d4beab12654af9d869e4e1cb39e2ee5c9
Nicholas <gidudunicholas11@gmail.com>
2023-05-19 17:37:12 +0300

    Request telephone number from front end

--- app/Models/CartPivot.php
+++ app/Models/CartPivot.php
@@ -10,22 +10,19 @@ class CartPivot extends Model
 {
     use HasFactory;
 
-
-    public $table = 'cart_pivots';
-
+    public $table = "cart_pivots";
 
     public $fillable = [
-        'cart_id',
-        'vendor_service_id',
-        'quantity',
-        'type',
-        'total_cost',
-        'user_id'
+        "cart_id",
+        "vendor_service_id",
+        "quantity",
+        "type",
+        "total_cost",
+        "user_id",
     ];
 
     public function product()
     {
-        return $this->belongsTo(VendorService::class,'vendor_service_id');
+        return $this->belongsTo(VendorService::class, "vendor_service_id");
     }
-
 }
