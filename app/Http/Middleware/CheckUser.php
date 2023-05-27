4aed83011a6ed777a2a2b8b145319a0a19e487cb
Nicholas <gidudunicholas11@gmail.com>
2023-05-27 08:24:47 +0300

    Google users to share one table with normal user

--- app/Http/Middleware/CheckUser.php
+++ app/Http/Middleware/CheckUser.php
@@ -18,32 +18,53 @@ class CheckUser
      */
     public function handle(Request $request, Closure $next)
     {
-        $Authorization = $request->header('Authorization');
-        if(empty($Authorization)){
+        $Authorization = $request->header("Authorization");
+        if (empty($Authorization)) {
             return response(
-            ['code'=>401,'message'=>"Authentication failed"],
-            401
-        );
+                ["code" => 401, "message" => "Authentication failed"],
+                401
+            );
         }
 
-        $access_token = trim(ltrim($Authorization, 'Bearer'));
-        $res_user = DB::table('google_users')->where('access_token', $access_token)->select('id','avatar','name','token','access_token','type','expiry_date')->first();
+        $access_token = trim(ltrim($Authorization, "Bearer"));
+        $res_user = DB::table("users")
+            ->where("access_token", $access_token)
+            ->select(
+                "id",
+                "avatar",
+                "name",
+                "token",
+                "access_token",
+                "type",
+                "expiry_date"
+            )
+            ->first();
 
-        if(empty($res_user)){
-            return response(['code'=>401,'message'=>'User does not exist'], 401);
+        if (empty($res_user)) {
+            return response(
+                ["code" => 401, "message" => "User does not exist"],
+                401
+            );
         }
         $expire_date = $res_user->expiry_date;
-        if(empty($expire_date)){
-            return response(['code'=>401,'message'=>'You must Login'], 401);
+        if (empty($expire_date)) {
+            return response(
+                ["code" => 401, "message" => "You must Login"],
+                401
+            );
         }
-        if($expire_date < Carbon::now()){
-            return response(['code'=>401,'message'=>'Your token has expired'], 401);
+        if ($expire_date < Carbon::now()) {
+            return response(
+                ["code" => 401, "message" => "Your token has expired"],
+                401
+            );
         }
         $addTime = Carbon::now()->addDays(5);
-        if($expire_date < $addTime){
+        if ($expire_date < $addTime) {
             $add_expire_date = Carbon::now()->addDays(30);
-            DB::table('google_users')->where('access_token',$access_token)
-            ->update(['expiry_date'=>$add_expire_date]);
+            DB::table("users")
+                ->where("access_token", $access_token)
+                ->update(["expiry_date" => $add_expire_date]);
         }
         $request->user_id = $res_user->id;
         $request->user_type = $res_user->type;
@@ -51,6 +72,6 @@ class CheckUser
         $request->user_avatar = $res_user->avatar;
         $request->user_token = $res_user->token;
 
-    return $next($request);
+        return $next($request);
     }
 }
